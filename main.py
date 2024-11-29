74d6a05aedbe2e6a6b7b7d8c2d2ddb577fefb056
khakimovallamurod <allamurod0206@gmail.com>
2024-11-29 13:23:53 +0200

    landmarks 68

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ detector = dlib.get_frontal_face_detector()
 # print type of detector
 print(type(detector))
 # Predictor for 5 face landmarks
-predictor = dlib.shape_predictor("shape_predictor_5_face_landmarks.dat")
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 # Get image from camera
 cap = cv2.VideoCapture(0)
@@ -17,9 +17,6 @@ while True:
     # Check if image is not empty
     if not ret:
         break
-
-    # Show image
-    cv2.imshow("frame", frame)
 
     # Convert to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -34,17 +31,23 @@ while True:
         left = face.left()
         right = face.right()
 
+        # Draw rectangle 
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 1)
+        # landmarks
+        landmarks = predictor(gray, face)
+        for n in range(0,68):
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y
+            frame = cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)
+        # Show image
+        cv2.imshow("frame", frame)
+        
         # Crop face
         face_img = frame[top:bottom, left:right]
         face_img = cv2.resize(face_img, (224, 224))
-        # landmarks
-        landmarks = predictor(gray, face)
-        for n in range(0,5):
-            x = landmarks.part(n).x
-            y = landmarks.part(n).y
-            face_img_landmark = cv2.circle(face_img, (x, y), 4, (255, 0, 0), -1)
-            # Show face
-            cv2.imshow("face_img_landmark", face_img_landmark)
+        
+        # Show face
+        cv2.imshow("face_img_landmark", face_img)
     # Wait for key press
     key = cv2.waitKey(1)
     # If key 'q' is pressed
